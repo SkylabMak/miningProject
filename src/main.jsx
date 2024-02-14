9a6c6dbd89b22c68064c1b9d8f80431bc03ffee8
SkylabMak <skylab135@gmail.com>
2024-02-14 17:53:15 +0700

    set up page

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import TestPage from './testPage/TestPage.jsx'
+
+export default function MainApp() {
+  return (
+    <BrowserRouter>
+      <Routes>
+          <Route index element={<App />} />
+          <Route path="testPage" element={<TestPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <MainApp />
 )
